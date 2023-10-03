073d101727188449384b044784cf362ec551afef
MunavarPM <munavarmunna101@gmail.com>
2023-10-03 10:28:30 +0530

    Initial Commit

---
+++ FashionStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FashionStore
+//
+//  Created by MUNAVAR PM on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FashionStore/FashionStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionStoreApp.swift
+//  FashionStore
+//
+//  Created by MUNAVAR PM on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FashionStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
