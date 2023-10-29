9f61ad6ac6f4de828e3707ece1b318cc430664dc
MunavarPM <munavarmunna101@gmail.com>
2023-10-29 13:54:57 +0530

    PaymentUI

---
+++ FashionStore/View/Payment.swift
@@ -0,0 +1,61 @@
+//
+//  Payment.swift
+//  FashionStore
+//
+//  Created by MUNAVAR PM on 29/10/23.
+//
+
+import SwiftUI
+
+struct Payment: View {
+    @State var isTap = false
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Payment")
+                    .font(.custom("PlayfairDisplay-Bold", size: 32).bold())
+                    .offset(x: -110)
+                Spacer()
+                VStack(alignment: .leading) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(isTap ? "Dark":"Light"))
+                            .frame(width: 370, height: 80)
+                            .shadow(radius: 4, x: 5, y: 6)
+                    }.overlay {
+                        HStack(spacing: 20) {
+                            Image(.modelFT)
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                            Text("Paypal")
+                                .foregroundStyle(Color(isTap ? "Light":"Dark"))
+                                .font(.custom("PlayfairDisplay-Bold", size: 20).bold())
+                            Spacer()
+                            Button {
+                                isTap.toggle()
+                            } label: {
+                                Image(systemName: isTap ? "circle.fill" : "circle")
+                                    .foregroundStyle(Color(isTap ? "Light":"Dark"))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    .background(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 2).opacity(0.1))
+                    Spacer()
+                }
+                .padding(.top)
+            }
+            
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    DismissView()
+                }
+            })
+        }
+    }
+}
+
+#Preview {
+    Payment()
+}
